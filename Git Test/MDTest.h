6f90b8818262db9ab5c684f12928258508f719a0
Matthew Danyeur <mdanyeur@microstrategy.com>
2014-04-22 10:20:31 -0700

    added test class

---
+++ Git Test/MDTest.h
@@ -0,0 +1,13 @@
+//
+//  MDTest.h
+//  Git Test
+//
+//  Created by Danyeur, Matthew on 4/22/14.
+//  Copyright (c) 2014 MicroStrategy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MDTest : NSObject
+
+@end
---
+++ Git Test/MDTest.m
@@ -0,0 +1,13 @@
+//
+//  MDTest.m
+//  Git Test
+//
+//  Created by Danyeur, Matthew on 4/22/14.
+//  Copyright (c) 2014 MicroStrategy. All rights reserved.
+//
+
+#import "MDTest.h"
+
+@implementation MDTest
+
+@end
