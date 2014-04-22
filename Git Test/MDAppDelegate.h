8a5da1f11fefa2364bffb9ed0f681a254cccd2a4
Matthew Danyeur <mdanyeur@microstrategy.com>
2014-04-22 10:00:48 -0700

    first commit

---
+++ Git Test/MDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MDAppDelegate.h
+//  Git Test
+//
+//  Created by Danyeur, Matthew on 4/22/14.
+//  Copyright (c) 2014 MicroStrategy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/MDViewController.h
@@ -0,0 +1,13 @@
+//
+//  MDViewController.h
+//  Git Test
+//
+//  Created by Danyeur, Matthew on 4/22/14.
+//  Copyright (c) 2014 MicroStrategy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDViewController : UIViewController
+
+@end
